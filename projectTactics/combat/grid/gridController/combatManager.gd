ae60c6b340aab7ac4b33ef6cdaf18b171b5c0353
Skiviper-AJM <adam.mcardlesep13@gmail.com>
2024-08-13 13:44:06 +1200

    Miscx

--- projectTactics/combat/grid/gridController/combatManager.gd
+++ projectTactics/combat/grid/gridController/combatManager.gd
@@ -13,31 +13,38 @@ const TILE_MATERIALS = [
 ]
 
 var highlighted_tiles := []
+var selected_unit_instance = null  # Store the instance of the selected unit
 
 func combatInitiate():
 	in_combat = true
 	player_combat_controller.block_placement = true  # Disable unit placement from inventory
 	print("Combat initiated. Unit selection from inventory disabled.")
 
-func _handle_unit_click(selected_unit):
+func _handle_unit_click(unit_instance):
 	if in_combat:
-		if player_combat_controller.currently_selected_tile:
-			player_combat_controller.currently_selected_tile.get_node("unit_hex/mergedBlocks(Clone)").material_override = TILE_MATERIALS[2]  # Set to red
+		if selected_unit_instance:
+			# Reset the previously selected tile color to red
+			player_combat_controller.currently_selected_tile.get_node("unit_hex/mergedBlocks(Clone)").material_override = TILE_MATERIALS[2]
 
 		clear_highlighted_tiles()
 
-		print("Selected unit type:", typeof(selected_unit), ", name:", selected_unit.name)
+		# Debugging
+		print("Switching to selected unit instance:", unit_instance)
 
-		# Set the selected unit in the DataPasser for movement
-		DataPasser.passUnitInfo(selected_unit)
-		player_combat_controller.unit_to_place = selected_unit
-		player_combat_controller.placing_unit = false
-		player_combat_controller.unit_name_label.text = "Unit: " + selected_unit.name
+		# Ensure the unit_instance is a Node3D and not a resource reference
+		if not unit_instance is Node3D:
+			print("Error: The selected unit is not a Node3D instance.")
+			return
 
-		# Find the tile the selected unit is currently on
+		# Set the instance as selected
+		selected_unit_instance = unit_instance
+
+		# Debugging to confirm the selection
+		print("Selected unit instance:", selected_unit_instance)
+
 		var selected_tile = null
 		for tile in player_combat_controller.units_on_tiles.keys():
-			if player_combat_controller.units_on_tiles[tile] == selected_unit:
+			if player_combat_controller.units_on_tiles[tile] == unit_instance:
 				selected_tile = tile
 				break
 
@@ -45,14 +52,14 @@ func _handle_unit_click(selected_unit):
 			selected_tile.get_node("unit_hex/mergedBlocks(Clone)").material_override = TILE_MATERIALS[1]  # Set to green
 			player_combat_controller.currently_selected_tile = selected_tile
 
-			highlight_tiles_around_unit(selected_unit, selected_unit.unitParts.speedRating)
-	else:
-		player_combat_controller.unitPlacer()
+			highlight_tiles_around_unit(selected_unit_instance, selected_unit_instance.unitParts.speedRating)
+		else:
+			print("Selected unit instance not found on any tile.")
 
-func highlight_tiles_around_unit(selected_unit, range):
+func highlight_tiles_around_unit(selected_unit_instance, range):
 	var unit_tile = null
 	for tile in player_combat_controller.units_on_tiles.keys():
-		if player_combat_controller.units_on_tiles[tile] == selected_unit:
+		if player_combat_controller.units_on_tiles[tile] == selected_unit_instance:
 			unit_tile = tile
 			break
 	
@@ -77,42 +84,36 @@ func clear_highlighted_tiles():
 
 func handle_tile_click(tile):
 	if in_combat:
-		if tile in highlighted_tiles and player_combat_controller.units_on_tiles.has(player_combat_controller.currently_selected_tile):
-			# Get the unit on the currently selected tile
-			var selected_unit = player_combat_controller.units_on_tiles[player_combat_controller.currently_selected_tile]
-
-			if selected_unit:
-				# Move the selected unit to the new tile
-				_move_unit_to_tile(selected_unit, tile)
-			else:
-				print("No unit found on the currently selected tile to move.")
+		if tile in highlighted_tiles and selected_unit_instance:
+			# Move the selected instance to the new tile
+			_move_unit_to_tile(selected_unit_instance, tile)
 		elif player_combat_controller.units_on_tiles.has(tile):
-			# Select the unit on the clicked tile
-			var unit_on_tile = player_combat_controller.units_on_tiles[tile]
-			_handle_unit_click(unit_on_tile)
+			# Select the unit instance on the clicked tile
+			var unit_instance = player_combat_controller.units_on_tiles[tile]
+			_handle_unit_click(unit_instance)
 		else:
 			print("Clicked tile is not highlighted for movement.")
 	else:
 		# Handle as necessary when not in combat
 		player_combat_controller.unitPlacer()
 
-func _move_unit_to_tile(selected_unit, target_tile):
-	# Ensure that selected_unit is a Node3D instance
-	if not selected_unit is Node3D:
-		print("Error: selected_unit is not a Node3D instance. Cannot move it.")
+func _move_unit_to_tile(selected_unit_instance, target_tile):
+	# Ensure that selected_unit_instance is a Node3D instance
+	if not selected_unit_instance is Node3D:
+		print("Error: selected_unit_instance is not a Node3D instance. Cannot move it.")
 		return
 	
 	# Directly move the specific instance on the map
-	print("Moving unit instance:", selected_unit, "to new tile:", target_tile)
+	print("Moving unit instance:", selected_unit_instance, "to new tile:", target_tile)
 
 	# Set the new unit's position to the target tile
-	selected_unit.position = target_tile.global_transform.origin
+	selected_unit_instance.position = target_tile.global_transform.origin
 
 	# Update the units_on_tiles dictionary to reflect the new tile
 	var old_tile = player_combat_controller.currently_selected_tile
 	if old_tile:
 		player_combat_controller.units_on_tiles.erase(old_tile)
-	player_combat_controller.units_on_tiles[target_tile] = selected_unit
+	player_combat_controller.units_on_tiles[target_tile] = selected_unit_instance
 
 	# Update the tile colors
 	if old_tile:
@@ -126,8 +127,6 @@ func _move_unit_to_tile(selected_unit, target_tile):
 	clear_highlighted_tiles()
 
 	# Deselect the unit after moving
-	DataPasser.passUnitInfo(null)
-	player_combat_controller.unit_to_place = null
-	player_combat_controller.placing_unit = false
+	selected_unit_instance = null
 	player_combat_controller.unit_name_label.text = ""
 	print("Unit moved to new tile successfully.")
